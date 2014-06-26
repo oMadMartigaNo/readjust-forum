158c6c2842843014a5104375639838c9612235ab
Fredrik Frodlund <fredrik.frodlund@jayway.com>
2014-06-26 16:38:37 +0200

    Add application Basic Pages

---
+++ applications/basicpages/settings/about.php
@@ -0,0 +1,34 @@
+<?php
+if (!defined('APPLICATION'))
+    exit();
+/**
+ * Basic Pages - An application for Garden & Vanilla Forums.
+ * Copyright (C) 2013  Shadowdare
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// An associative array of information about this application.
+$ApplicationInfo['BasicPages'] = array(
+    'Name' => 'Basic Pages',
+    'Description' => "Basic Pages is an application that provides a way for you to create basic public pages for static content in Garden.",
+    'Version' => '2.1.3',
+    'RequiredApplications' => array('Vanilla' => '2.1'),
+    'Author' => "Shadowdare",
+    'AuthorUrl' => 'http://vanillaforums.org/profile/addons/16014/Shadowdare',
+    'Url' => 'http://vanillaforums.org/addon/basicpages-application',
+    'License' => 'GPLv3',
+    'SetupController' => 'setup',
+    'SettingsUrl' => 'pagessettings/allpages'
+);
