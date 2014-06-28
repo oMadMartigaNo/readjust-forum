37656128d3a65c6397cc560fe8a33f182c0189e1
Fredrik Frodlund <frippz@me.com>
2014-06-28 11:49:19 +0200

    Code cleanup

--- plugins/reAdjust/default.php
+++ plugins/reAdjust/default.php
@@ -17,13 +17,11 @@ $PluginInfo['reAdjust'] = array(
  */
 class readjustPlugin extends Gdn_Plugin {
   
-  public function Base_Render_Before(&$Sender){
+  public function Base_Render_Before($Sender){
     
     // Only render outside the dashboard
     if ($Sender->MasterView != 'admin') {
-      
       $Sender->AddCssFile('/plugins/reAdjust/design/readjust.css');
-      
     }
     
   }
