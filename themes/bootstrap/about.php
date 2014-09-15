afba234f6812c9acbde2040cb3ca6502d31b1b47
Fredrik Frodlund <frippz@me.com>
2014-09-15 08:59:03 +0200

    Upgrade Bootstrap theme to v2.3.2

--- themes/bootstrap/about.php
+++ themes/bootstrap/about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['Bootstrap'] = array(
   'Name'        => 'Bootstrap',
   'Description' => "Bootstrap for Vanilla has been rewritten from scratch on top of <a href='http://getbootstrap.com'>Bootstrap 3</a> in this second installation of the most popular theme for Vanilla Forums. Kickstart your community with a fresh and solid theme that is ready to be customized to your heart's desires.",
-  'Version'     => '2.3.1',
+  'Version'     => '2.3.2',
   'Url'         => 'https://github.com/kasperisager/vanilla-bootstrap',
   'Author'      => 'Kasper Kronborg Isager',
   'AuthorEmail' => 'kasperisager@gmail.com',
@@ -23,7 +23,9 @@ $ThemeInfo['Bootstrap'] = array(
       'Flatly'    => '%s_flatly',
       'Journal'   => '%s_journal',
       'Lumen'     => '%s_lumen',
+      'Paper'     => '%s_paper',
       'Readable'  => '%s_readable',
+      'Sandstone' => '%s_sandstone',
       'Simplex'   => '%s_simplex',
       'Slate'     => '%s_slate',
       'Spacelab'  => '%s_spacelab',
