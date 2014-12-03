7af4d7010353e8224cb4fb19db25c351111a2672
Fredrik Frodlund <frippz@me.com>
2014-12-03 12:45:51 +0100

    Re-install plugin Timeago

---
+++ plugins/Timeago/locale/en-CA.php
@@ -0,0 +1,18 @@
+<?php
+
+$Definition['timeago.prefixAgo']     = null;
+$Definition['timeago.prefixFromNow'] = null;
+$Definition['timeago.suffixAgo']     = 'ago';
+$Definition['timeago.suffixFromNow'] = 'from now';
+$Definition['timeago.seconds']       = 'less than a minute';
+$Definition['timeago.minute']        = 'about a minute';
+$Definition['timeago.minutes']       = '%d minutes';
+$Definition['timeago.hour']          = 'about an hour';
+$Definition['timeago.hours']         = 'about %d hours';
+$Definition['timeago.day']           = 'a day';
+$Definition['timeago.days']          = '%d days';
+$Definition['timeago.month']         = 'about a month';
+$Definition['timeago.months']        = '%d months';
+$Definition['timeago.year']          = 'about a year';
+$Definition['timeago.years']         = '%d years';
+$Definition['timeago.wordSeparator'] = ' ';
