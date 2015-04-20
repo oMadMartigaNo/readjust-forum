9cff96033d2cb62a869e3e81ad4ce26663e32a93
Fredrik Frodlund <frippz@me.com>
2015-04-20 13:15:04 +0200

    Remove whitespace

--- plugins/reAdjust/default.php
+++ plugins/reAdjust/default.php
@@ -16,26 +16,27 @@ $PluginInfo['reAdjust'] = array(
  * Begin plugin
  */
 class readjustPlugin extends Gdn_Plugin {
-  
+
   public function Base_Render_Before($Sender) {
-    
+
     // Only render outside the dashboard
     if ($Sender->MasterView != 'admin') {
       $Sender->AddCssFile('/plugins/reAdjust/design/readjust.css');
     }
-    
+
   }
-  
+
   public function PageController_Render_Before($Sender) {
-    
+
     // Hide unwanted modules
     unset($Sender->Assets['Panel']['DiscussionsModule']);
     unset($Sender->Assets['Panel']['RecentActivityModule']);
-    
+
   }
-  
+
   public function Setup() {
     // No setup required
   }
-  
+
 }
+
